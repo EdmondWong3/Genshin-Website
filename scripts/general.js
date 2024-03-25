d8e3eb6b2913d5eb5b8f4ffde83e432c6f255857
EdmondWong3 <edmonwong12@gmail.com>
2024-03-25 08:15:49 -0400

    WEBP test

--- scripts/general.js
+++ scripts/general.js
@@ -10,5 +10,5 @@ document.addEventListener("DOMContentLoaded", function() {
     content.style.display = "block";
   }
 
-  setTimeout(hideLoadingScreen, 1000);
+  setTimeout(hideLoadingScreen, 1500);
 })
